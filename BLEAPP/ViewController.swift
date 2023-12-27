8a1e1f3d135b9593cc09e6a68e0cb2f135b0f21c
DevKDuck <loading95@naver.com>
2023-12-27 19:42:09 +0900

    Initial Commit

---
+++ BLEAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLEAPP
+//
+//  Created by duck on 2023/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
